687f9c7a6f4eb4dbbb12aa9d6a0b3bb68ce4bc8a
NobodyIsThere <as1129@york.ac.uk>
2017-09-12 17:28:21 +0100

    Added convolutional example.

--- convolutional_example.py
+++ convolutional_example.py
@@ -2,8 +2,9 @@ import data_provider as dp
 import tensorflow as tf
 
 # Parameters
-hidden_size = 200       # Number of units in the hidden layer
-learning_rate = 0.01    # Learning rate for gradient descent optimiser
+num_fmaps = 4           # Number of feature maps for the convolutional layer
+connected_size = 200    # Number of units in the fully connected layer
+learning_rate = 1e-4    # Learning rate for the Adam optimiser
 num_epochs = 10         # Number of training epochs
 batch_size = 100        # Number of training examples per batch
 
@@ -21,32 +22,41 @@ target_length = DP.target_length
 x = tf.placeholder(tf.float32, shape=[None, input_length], name="input")
 t = tf.placeholder(tf.float32, shape=[None, target_length], name="target")
 
-# Weights and biases for hidden layer
-Wh = tf.Variable(tf.truncated_normal([input_length, hidden_size], stddev=0.1))
-bh = tf.Variable(tf.constant(0.1, shape=[hidden_size]))
+# Reshape input from a vector into two grids
+reshaped_input = tf.reshape(x, [-1, 2, 7, 6])
+input_layer = tf.transpose(reshaped_input, perm=[0, 2, 3, 1])
+# The dimensions of input_layer now correspond to [batch, width, height, player]
 
-# Weights and biases for the output layer
-Wo = tf.Variable(tf.truncated_normal([hidden_size, target_length], stddev=0.1))
+# Convolutional layer
+Wc = tf.Variable(tf.truncated_normal([4, 4, 2, num_fmaps], stddev=0.1))
+bc = tf.Variable(tf.constant(0.1, shape=[num_fmaps]))
+
+conv_layer = tf.nn.relu(tf.nn.conv2d(input_layer, Wc, strides=[1, 1, 1, 1],
+                                                      padding='SAME') + bc)
+
+# Fully connected layer
+Wfc = tf.Variable(tf.truncated_normal([7*6*num_fmaps, connected_size],
+                                      stddev=0.1))
+bfc = tf.Variable(tf.constant(0.1, shape=[connected_size]))
+
+flattened_convolution = tf.reshape(conv_layer, [-1, 7*6*num_fmaps])
+connected_layer = tf.nn.relu_layer(flattened_convolution, Wfc, bfc)
+
+# Output layer
+Wo = tf.Variable(tf.truncated_normal([connected_size, target_length],
+                                     stddev=0.1))
 bo = tf.Variable(tf.constant(0.1, shape=[target_length]))
 
-# Layers
-hidden_layer = tf.nn.relu_layer(x, Wh, bh)
-output_layer = tf.matmul(hidden_layer, Wo) + bo
+output_layer = tf.matmul(connected_layer, Wo) + bo
 
-# We have built the network. Now specify loss function for training.
+# Specify loss and train network as before, this time using Adam optimiser.
+# (Gradient descent would also work fine.)
 cross_entropy = tf.reduce_mean(
     tf.nn.softmax_cross_entropy_with_logits(labels=t, logits=output_layer))
-
-# We want to minimise the loss, using gradient descent. Every time we evaluate
-# train_op, the optimiser will make one step.
-train_op = tf.train.GradientDescentOptimizer(
-    learning_rate).minimize(cross_entropy)
-
-# Loss is not very interpretable, so calculate the accuracy.
-correct = tf.equal(tf.argmax(output_layer, 1), tf.argmax(t, 1))
+train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+correct = tf.equal(tf.argmax(t, 1), tf.argmax(output_layer, 1))
 accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
-# Now actually train the network!
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     
